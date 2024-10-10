0fc96e486fd253b585b9db207ee5f56ae2839e86
jpaynedevelopment <38086642+jpaynedevelopment@users.noreply.github.com>
2024-10-10 10:15:21 -0700

    added screens for the app within the layout folder

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/CalenderActivity.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/CalenderActivity.kt
@@ -1,26 +1,20 @@
 package edu.arizona.ISTA498.trektraveler
-import android.content.Intent
+
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CalenderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.cal_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun goToCalendar(view: View){
-        val intent = Intent(this, CalenderActivity::class.java)
-        startActivity(intent)
     }
 }
