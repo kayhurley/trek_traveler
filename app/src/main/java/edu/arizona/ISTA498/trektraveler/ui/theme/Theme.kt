2902b5580728fd1d11f4fae60fcc6b9b88c62945
kayhurley <126208740+kayhurley@users.noreply.github.com>
2024-10-17 09:29:11 -0700

    Search bars -> buttons

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/ui/theme/Theme.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/ui/theme/Theme.kt
@@ -18,9 +18,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Teal,
+    secondary = Teal,
+    tertiary = Teal
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
