68ef4eaf4161c778f8752e7d685d9c91bb7e86d5
zhantommy54 <zhantommy54@arizona.edu>
2024-10-28 14:36:42 -0700

    This is walk score & bike score & transit score data

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/MainActivity.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/MainActivity.kt
@@ -26,74 +26,79 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-   @OptIn(DelicateCoroutinesApi::class)
-   fun get_walk_score_data() {
-       GlobalScope.launch(Dispatchers.IO) {
-           try {
-               val response =
-                   URL("https://api.walkscore.com/score?format=json&lat=32.2226&lon=-110.9747&transit=1&bike=1&wsapikey=d874b2705cbb91f53a74fd1003d74fdf").readText()
-               val jsonresponse = parseObject(response)
-               val walk_score = jsonresponse?.getInt("walkscore")
-               val transit = jsonresponse?.getJSONObject("transit")
-               val transit_score = transit?.getInt("score")
-               val bikes = jsonresponse?.getJSONObject("bike")
-               val bike_score = bikes?.getInt("score")
-               print("Walk Score is: " + walk_score)
-               print("Transit Score is: " + transit_score)
-               print("Bike Score is: " + bike_score)
-           } catch (e: java.lang.Exception) {
-               print(e.message)
-           }
-       }
-   }
-   fun get_weather_score_data() {
-       GlobalScope.launch(Dispatchers.I0) {
-           try {
-               val response =
-                   URL("https://api.openweathermap.org/data/2.5/weather?lat=32.2226&lon=-110.9747&appid=772a8fce0fd1f9e51c34f53b07845329").readText()
-               val jsonresponse = parseObject(response)
-               val weather = jsonresponse?.getJSONArray("weather")
+    @OptIn(DelicateCoroutinesApi::class)
+    fun get_walk_score_data() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response =
+                    URL("https://api.walkscore.com/score?format=json&lat=32.2226&lon=-110.9747&transit=1&bike=1&wsapikey=d874b2705cbb91f53a74fd1003d74fdf").readText()
+                val jsonresponse = parseObject(response)
+                val walk_score = jsonresponse?.getInt("walkscore")
+                val transit = jsonresponse?.getJSONObject("transit")
+                val transit_score = transit?.getInt("score")
+                val bikes = jsonresponse?.getJSONObject("bike")
+                val bike_score = bikes?.getInt("score")
+                print("Walk Score is: " + walk_score)
+                print("Transit Score is: " + transit_score)
+                print("Bike Score is: " + bike_score)
+            } catch (e: java.lang.Exception) {
+                print(e.message)
+            }
+        }
+    }
+    @OptIn(DelicateCoroutinesApi::class)
+    fun get_weather_score_data() {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response =
+                    URL("https://pro.openweathermap.org/data/2.5/weather?q=Arizona,us&APPID=772a8fce0fd1f9e51c34f53b07845329&units=imperial").readText()
+                val jsonresponse = parseObject(response)
+                val weather = jsonresponse?.getJSONArray("weather")!!
                 if (weather.length() > 0) {
-                    val weather_object = weather[0]
+                    val weather_object = weather[0] as JSONObject
                     val weather_main = weather_object.getString("main")
                     val weather_description = weather_object.getString("description")
+                    val weather_icon = weather_object.getString("icon")
                 }
+                val main_object = jsonresponse.getJSONObject("main")
+                val temp_min = main_object.getDouble("temp_min")
+                val temp_max = main_object.getDouble("temp_max")
+                print("Max Temperature is: " + temp_max)
+                print("Min Temperature is: " + temp_min)
+            } catch (e: java.lang.Exception) {
+                print(e.message)
+            }
 
+        }
 
-           } catch (e: java.lang.Exception) {
-               print(e.message)
-           }
-
-       }
-
-
-fun parseObject(json: String): JSONObject? {
-    var jsonObject: JSONObject? = null
-    try {
-        jsonObject = JSONObject(json)
-    } catch (e: JSONException) {
-        e.printStackTrace()
     }
-    return jsonObject
-}
-
-fun parseArray(json: String): JSONArray? {
-    var jsonArray: JSONArray? = null
-    try{
-        jsonArray = JSONArray(json)
-    }catch (e: JSONException){
-        e.printStackTrace()
+    fun parseObject(json: String): JSONObject? {
+        var jsonObject: JSONObject? = null
+        try {
+            jsonObject = JSONObject(json)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        return jsonObject
     }
 
-    return jsonArray
-}
+    fun parseArray(json: String): JSONArray? {
+        var jsonArray: JSONArray? = null
+        try {
+            jsonArray = JSONArray(json)
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
 
-    fun goToCalendar(view: View){
+        return jsonArray
+    }
+
+    fun goToCalendar(view: View) {
         val intent = Intent(this, CalenderActivity::class.java)
         startActivity(intent)
     }
 
-    fun goToCityPage(view: View){
+    fun goToCityPage(view: View) {
         val intent = Intent(this, CityActivity::class.java)
         startActivity(intent)
     }
