5108558c746dbec7ad4f05d1651134e4f7f6ab8b
kayhurley <126208740+kayhurley@users.noreply.github.com>
2024-11-09 15:56:38 -0700

    attach links

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/InfoTab.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/InfoTab.kt
@@ -1,10 +1,13 @@
 package edu.arizona.ISTA498.trektraveler
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 
 class InfoTab : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,16 @@ class InfoTab : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.info_screen, container, false)
+        val view = inflater.inflate(R.layout.info_screen, container, false)
+
+        // Set up the MaterialButton click listener
+        val walkscoreLink = view.findViewById<MaterialButton>(R.id.walkscoreLink)
+        walkscoreLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.walkscore.com/methodology.shtml"))
+            startActivity(intent)
+        }
+
+        return view
     }
+
 }
