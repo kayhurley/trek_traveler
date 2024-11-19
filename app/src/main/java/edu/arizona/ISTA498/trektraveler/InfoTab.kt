2955ac26f470098f3638cea12bc9763bbbc59766
kayhurley <126208740+kayhurley@users.noreply.github.com>
2024-11-19 16:36:31 -0700

    readded button code

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/InfoTab.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/InfoTab.kt
@@ -1,5 +1,7 @@
 package edu.arizona.ISTA498.trektraveler
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.text.SimpleDateFormat
@@ -19,8 +22,17 @@ class InfoTab : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.info_screen, container, false)
 
+        // Set up the MaterialButton click listener for the WalkScore link
+        val walkscoreLink = view.findViewById<MaterialButton>(R.id.walkscoreLink)
+        walkscoreLink.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.walkscore.com/methodology.shtml"))
+            startActivity(intent)
+        }
+
+        // Find UI elements
         val highLowTextView: TextView = view.findViewById(R.id.highLowText)
         val typicalWeatherText: TextView = view.findViewById(R.id.typicalWeatherText)
         val weatherIcon: ImageView = view.findViewById(R.id.weatherIcon) // Replace with your actual ID
@@ -41,6 +53,7 @@ class InfoTab : Fragment() {
 
         return view
     }
+
 
     private fun updateTypicalWeatherText(userSelectedDate: String, typicalWeatherText: TextView) {
         try {
@@ -157,4 +170,7 @@ class InfoTab : Fragment() {
     }
 
     private data class WeatherData(val date: String, val maxTemp: Double, val minTemp: Double)
+
+
 }
+
