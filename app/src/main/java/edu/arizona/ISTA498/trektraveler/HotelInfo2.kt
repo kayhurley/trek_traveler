c3043e7e4bba3c664f74b4b25ae098a7b060578b
zhantommy54 <zhantommy54@arizona.edu>
2024-12-02 14:49:15 -0700

    Added a second hotel to the accomodations tab with all the information changed

--- app/src/main/java/edu/arizona/ISTA498/trektraveler/HotelInfo2.kt
+++ app/src/main/java/edu/arizona/ISTA498/trektraveler/HotelInfo2.kt
@@ -1,19 +1,17 @@
 package edu.arizona.ISTA498.trektraveler
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HotelActivity : AppCompatActivity() {
+class HotelInfo2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        setContentView(R.layout.hotel)
+        setContentView(R.layout.activity_hotel_info_2)
 
         // Set up the app bar for back navigation
         val toolbar = findViewById<Toolbar>(R.id.toolbar) // Ensure your layout has a Toolbar with this ID
@@ -30,19 +28,6 @@ class HotelActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Add click listener to the hotelInfoButton
-        val hotelInfoButton = findViewById<Button>(R.id.hotelInfoButton)
-        hotelInfoButton.setOnClickListener {
-            val intent = Intent(this, HotelInfo::class.java)
-            startActivity(intent)
-        }
-        // Add click listener to the hotelInfoButton
-        val hotelInfoButton2 = findViewById<Button>(R.id.hotel2InfoButton)
-        hotelInfoButton2.setOnClickListener {
-            val intent = Intent(this, HotelInfo2::class.java)
-            startActivity(intent)
         }
     }
 }
